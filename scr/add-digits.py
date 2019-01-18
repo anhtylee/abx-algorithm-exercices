2076bf47f8f3e26e804bb847f5f1223e3726fb98
anhty <anhty9le@gmail.com>
2019-01-18 18:48:14 +0700

    delete

--- scr/add-digits.py
+++
@@ -1,7 +0,0 @@
-#url https://leetcode.com/problems/add-digits/
-#writtenby:anhty9le	
-
-class Solution(object):
-    def addDigits(self,num):
-        if num<10: return num
-        else: return self.addDigits(num%10+self.addDigits((num//10)))
